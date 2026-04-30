7ed7880b2275a3c6605862089dd756fddc85be14
mdrifatdev <rifataslam1212@gmail.com>
2026-05-01 00:29:11 +0600

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
   const [count, setCount] = useState(0)
